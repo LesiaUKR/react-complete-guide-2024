45c1e7fbc00313a47945b8aa06864e45e77503be
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-10-03 21:02:51 +0100

    tic tac toe game board added

--- section_4_tic_tac_toe/src/App.jsx
+++ section_4_tic_tac_toe/src/App.jsx
@@ -1,16 +1,49 @@
-import Player from "./components/Player"
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
 
 function App() {
-  return <main>
-<div id="game-container">
-<ol id="players">
- <Player initialName="Player 1" symbol="X" />
- <Player initialName="Player 2" symbol="0" />
-</ol>
-GAME BOARD
-</div>
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "0" : "X"));
+    setGameTurns((prevGameTurns) => {
+      let currentPlayer = 'X';
 
-  </main>
+      if (prevGameTurns.length > 0 && prevGameTurns[0].player === 'X'){
+        currentPlayer = '0';
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevGameTurns,
+      ];
+      return updatedTurns;
+    });
+  }
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="0"
+            isActive={activePlayer === "0"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+          turns={gameTurns}
+        />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- section_4_tic_tac_toe/src/components/Player.jsx
+++ section_4_tic_tac_toe/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -32,7 +32,7 @@ export default function Player({ initialName, symbol }) {
   }
   
   return (
-    <li>
+    <li className={isActive?'active':undefined}>
       <span className="player">
         {/* якщо isEditing === true, то відобразити input, інакше - span */}
         {/* {isEditing && <input type="text" value={name} required/>}
