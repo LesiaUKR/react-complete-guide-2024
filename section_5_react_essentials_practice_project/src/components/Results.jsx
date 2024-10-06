7a5d95313ac48b51b943119927de66fb3d8f7e0f
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-10-06 21:52:21 +0100

    add table results to the Investment calculator

--- section_5_react_essentials_practice_project/src/components/Results.jsx
+++ section_5_react_essentials_practice_project/src/components/Results.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
-import { calculateInvestmentResults } from './../util/investment';
+import React from "react";
+import { calculateInvestmentResults, formatter } from "./../util/investment";
 
-export default function Results({input}) {
-  
-   const resultsData = calculateInvestmentResults(input);
-   console.log(resultsData)
+export default function Results({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
   return (
- <table id="results">
-   <thead>
-      <tr>
-         <th>Year</th>
-         <th>Investment Value</th>
-         <th>Interest (Year)</th>
-         <th>Invested Capital</th>
-      </tr>
-   </thead>
- </table>
-  )
+    <table id="results">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+            const totalAmountInvested =
+            yearData.valueEndOfYear -
+            totalInterest;
+
+          return (
+            <tr>
+              <td key={yearData.year}>{yearData.year}</td>
+              <td key={yearData.year}>
+                {formatter.format(yearData.valueEndOfYear)}
+              </td>
+              <td key={yearData.year}>{totalInterest}</td>
+              <td key={yearData.year}>{formatter.format(yearData.interest)}</td>
+              <td key={yearData.year}>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
