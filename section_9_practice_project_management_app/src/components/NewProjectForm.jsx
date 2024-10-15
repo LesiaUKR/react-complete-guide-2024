3fdf3ed37529ae1cfa8eb808bd57edc336b336f5
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-10-15 19:02:08 +0100

    finish Management App

--- section_9_practice_project_management_app/src/components/NewProjectForm.jsx
+++ section_9_practice_project_management_app/src/components/NewProjectForm.jsx
@@ -1,7 +1,9 @@
 import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-export default function NewProjectForm({ onAdd }) {
+export default function NewProjectForm({ onAdd, onCancel }) {
+  const modal= useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,6 +13,14 @@ const enteredTitle = title.current.value;
 const enteredDescription = description.current.value;
 const enteredDueDate = dueDate.current.value;
 // validation ...
+if (
+  enteredTitle.trim() === '' ||
+  enteredDescription.trim() === '' ||
+  enteredDueDate.trim() === ''
+) {
+ modal.current.open();
+ return;
+}
 
 onAdd({
   title: enteredTitle,
@@ -20,10 +30,16 @@ onAdd({
 }
 
   return (
+    <>
+    <Modal ref={modal} buttonCaption="Okay">
+      <h2 className="text-xl font-bold text-stone-700 my-4">Invalid input</h2>
+      <p  className="text-stone-600 mb-4">Ooops ... looks like you forgot to enter a value.</p>
+      <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
+    </Modal>
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button onClick={onCancel} className="text-stone-800 hover:text-stone-950">
               Cancel
             </button>
           </li>
@@ -37,5 +53,6 @@ onAdd({
       <Input  type="text" ref={description} label="Description" textarea />
       <Input type="date" ref={dueDate} label="Due Date" />
     </div>
+    </>
   );
 }
--- section_9_practice_project_management_app/src/components/TaskItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TaskItem() {
-  return (
-    <li>
-      Task 1
-    </li>
-  )
-}
